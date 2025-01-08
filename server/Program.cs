f3df73a178ee3cb6c2c30086476e5b5ae7ce3de9
Oskar Z <oski.zet99@gmail.com>
2025-01-08 09:08:21 +0100

    Update CORS rules

--- server/Program.cs
+++ server/Program.cs
@@ -8,12 +8,22 @@ namespace Server {
       WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
       builder.Services.AddControllers();
       
+      builder.Services.AddCors(options => {
+        options.AddPolicy("allow", policyBuilder => {
+          policyBuilder.AllowAnyHeader();
+          policyBuilder.AllowAnyMethod();
+          policyBuilder.AllowCredentials();
+          policyBuilder.WithOrigins("http://localhost:5173", "https://localhost:5173");
+        });
+      });
+
       builder.Services.AddDbContext<SQLiteContext>(options => {
         options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnectionString"));
       });
 
       WebApplication app = builder.Build();
       app.MapControllers();
+      app.UseCors("allow");
       app.Run();
 
     }
